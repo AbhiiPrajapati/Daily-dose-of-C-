5e08340372e2d97871b3150991c3902c0073e89a
ItaAviPrajapati <sirabhishekprajapati@gmail.com>
2024-05-03 00:01:03 +0530

    binary

---
+++ binarycoeff.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int fact(int n)
+{
+  int factorial = 1;
+  for (int i = 2; i <= n; i++)
+  {
+    factorial *= i;
+  }
+  return factorial;
+}
+
+int main()
+{
+  int n, r, ans;
+  cout << "enter n and r of nCr : ";
+  cin >> n >> r;
+  ans = (fact(n) / (fact(n - r) * fact(r)));
+  cout << ans;
+  return 0;
+}
