de1d63a56a33c00327a34357935ae51de4432073
ItaAviPrajapati <sirabhishekprajapati@gmail.com>
2024-05-02 23:17:57 +0530

    love fibonacci

---
+++ fibonacci.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void fib(int n)
+{
+  int temp1 = 0;
+  int temp2 = 1;
+  int total;
+  for (int i = 1; i <= n; i++)
+  {
+    total = temp1 + temp2;
+    cout << total << endl;
+    temp1 = temp2;
+    temp2 = total;
+  }
+
+  return;
+}
+
+int main()
+{
+  int n;
+  cout << " Enter the num untill whom u want to see FIBONICCI series ";
+  cin >> n;
+  fib(n);
+  return 0;
+}
---
+++ voidsum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void sum(int a, int b)
+{
+
+  cout << a + b;
+  return;
+}
+
+main()
+{
+  int a = 5, b = 6;
+
+  sum(a, b);
+
+  return 0;
+}
