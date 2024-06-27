2949757c9e7e350a7b807c6f4add7f32c8a96b65
ItaAviPrajapati <sirabhishekprajapati@gmail.com>
2024-06-27 23:40:42 +0530

    updated

--- array.cpp
+++ array.cpp
@@ -1,12 +1,13 @@
 /* TOPIC  ARRAYS
 Arrays is a list of variables of similar kind
 */
-
+// sei jskaljals
 #include <iostream>
 using namespace std;
 int main()
 {
   int array[5] = {23, 45, 6, 22, 1};
   cout << array[2];
+  cout << "its an array";
   return 0;
 }
