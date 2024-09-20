cb2f5c4eeadba0da1dca852d56295af6d5d15474
Avi Prajapati <118271762+AbhiiPrajapati@users.noreply.github.com>
2024-09-20 23:07:28 +0530

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -6,7 +6,7 @@ Arrays is a list of variables of similar kind
 using namespace std;
 int main()
 {
-cout<<" -:+: THIS IS AN ARRAY PROGRAM :+:- "<<endl;
+cout<<" -:: THIS IS AN ARRAY PROGRAM ::- "<<endl;
   int array[5] = {23, 45, 6, 22, 1};
   cout << array[2];
   cout << "its an array at index 2";
